da0610aff47f2674a5a360cfddb8d2382201f8f5
KevinRoy <luozheng216@gmail.com>
2016-08-26 16:51:33 +0800

    update two Shell

---
+++ KGHosts.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+result = $(curl https://raw.githubusercontent.com/racaljk/hosts/master/hosts)
+echo result
---
+++ PolyhomeUninstallOther.sh
@@ -0,0 +1,8 @@
+echo "uninstall com.techjumper.polyhome.b.home"
+adb uninstall com.techjumper.polyhome.b.home
+echo "uninstall com.techjumper.polyhome.b.info"
+adb uninstall com.techjumper.polyhome.b.info
+echo "uninstall com.techjumper.polyhome.b.property"
+adb uninstall com.techjumper.polyhome.b.property
+echo "uninstall com.techjumper.polyhome.b.setting"
+adb uninstall com.techjumper.polyhome.b.setting
